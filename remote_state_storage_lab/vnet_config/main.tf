54a28df7fe0f4353dd87d02e21a4a22250a9489a
weoamathior <weoamathior@gmail.com>
2024-12-03 13:17:00 -0800

    add a few subnets

--- remote_state_storage_lab/vnet_config/main.tf
+++ remote_state_storage_lab/vnet_config/main.tf
@@ -14,9 +14,25 @@ data "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "taco-wagon-app"
+  name                = "vnet1"
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
 
-  address_space = ["10.42.0.0/24"]
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "default" {
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/24"]
+  name                 = "default"
+  resource_group_name  = data.azurerm_resource_group.main.name
+
+}
+
+resource "azurerm_subnet" "ServerSubnet" {
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+  name                 = "ServerSubnet"
+  resource_group_name  = data.azurerm_resource_group.main.name
+
 }
